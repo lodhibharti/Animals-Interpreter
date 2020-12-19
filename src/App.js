c5c52d9cb66b9df33d216b9795f73a297f3b9da6
Bharti Lodhi <62990284+lodhibharti@users.noreply.github.com>
2020-12-19 09:48:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "🐵": " Monkey Face",
+  "🐒": "Monkey",
+  "🦍": "Gorilla",
+  "🦧": "Orangutan",
+  "🐶": "Dog Face",
+  "🐕": "Dog",
+  "🦮": "Guide Dog",
+  "🐕‍🦺": "Service Dog",
+  "🐩": "Poodle",
+  "🐺": "Wolf",
+  "🦊": "Fox",
+  "🦝": "Raccoon",
+  "🐱": "Cat Face",
+  "🐈": "Cat",
+  "🦁": "Lion",
+  "🐯": "Tiger Face",
+  "🐅": "Tiger",
+  "🐆": "Leopard",
+  "🐴": "Horse Face",
+  "🐎": "Horse",
+  "🦄": "Unicorn",
+  "🦓": "Zebra",
+  "🦌": "Deer",
+  "🐮": "Cow Face",
+  "🐂": "Ox",
+  "🐃": "Water Buffalo",
+  "🐄": "Cow",
+  "🐷": "Pig Face",
+  "🐖": "Pig",
+  "🐗": "Boar",
+  "🐏": "Ram",
+  "🐑": "Ewe",
+  "🐐": "Goat",
+  "🐪": "Camel",
+  "🐫": "Two-Hump Camel",
+  "🦙": "Llama",
+  "🦒": "Giraffe",
+  "🐘": "Elephant",
+  "🦏": "Rhinoceros",
+  "🦛": "Hippopotamus",
+  "🐭": "Mouse Face",
+  "🐁": "Mouse",
+  "🐀": "Rat",
+  "🐹": "Hamster",
+  "🐰": "Rabbit Face",
+  "🐇": "Rabbit",
+  "🐿️": "Chipmunk",
+  "🦔": "Hedgehog",
+  "🦇": "Bat",
+  "🐻": "Bear",
+  "🐻‍": "Polar Bear",
+  "🐨": "Koala",
+  "🐼": "Panda",
+  "🦥": "Sloth",
+  "🦦": "Otter",
+  "🦨": "Skunk",
+  "🦘": "Kangaroo",
+  "🦡": "Badger",
+  "🐾": "Paw Prints",
+  "🦃": "Turkey",
+  "🐔": "Chicken",
+  "🐓": "Rooster",
+  "🐣": "Hatching Chick",
+  "🐤": "Baby Chick",
+  "🐥": "Front-Facing Baby Chick",
+  "🐦": "Bird",
+  "🐧": "Penguin",
+  "🕊️": "Dove",
+  "🦅": "Eagle",
+  "🦆": "Duck",
+  "🦢": "Swan",
+  "🦉": "Owl",
+  "🦩": "Flamingo",
+  "🦚": "Peacock",
+  "🦜": "Parrot",
+  "🐸": "Frog",
+  "🐊": "Crocodile",
+  "🐢": "Turtle",
+  "🦎": "Lizard",
+  "🐍": "Snake",
+  "🐲": "Dragon Face",
+  "🐉": "Dragon",
+  "🦕": "Sauropod",
+  "🦖": "T-Rex",
+  "🐳": "Spouting Whale",
+  "🐋": "Whale",
+  "🐬": "Dolphin",
+  "🐟": "Fish",
+  "🐠": "Tropical Fish",
+  "🐡": "Blowfish",
+  "🦈": "Shark",
+  "🐙": "Octopus",
+  "🐚": "Spiral Shell",
+  "🐌": "Snail",
+  "🦋": "Butterfly",
+  "🐛": "Bug",
+  "🐜": "Ant",
+  "🐝": "Honeybee",
+  "🐞": "Lady Beetle",
+  "🦗": "Cricket",
+  "🕷️ ": "Spider",
+  "🕸️": "Spider Web",
+  "🦂": "Scorpion",
+  "🦟": "Mosquito"
+};
+
+//console.log(Object.keys(emojiDictionary));
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello! Welcome</h1>
+      <h2>Animals Emoji</h2>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2>{meaning}</h2>
+
+      <h3>Emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
